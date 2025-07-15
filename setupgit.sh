5a6192899c2c7bde91d9d9225bc85f46ed01edd4
LuxorAdesto <your.email@example.com>
2025-07-15 16:55:09 -0700

    lets go!

---
+++ setupgit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+git config --global user.name "LuxorAdesto"
+git config --global user.email "your.email@example.com"
+
+
+git remote set-url origin https://ghp_MsO1N71faiG0jIBIdx2Z1E8XisNhA2Apu6@github.com/LuxorAdesto/Asherunner.git
